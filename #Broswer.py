f489e533d1d2fa1cc8caf14b7c9c7bf235099f02
heliohenriquehfilho <81873644+heliohenriquehfilho@users.noreply.github.com>
2021-06-20 13:04:14 -0300

    Update #Broswer.py

--- #Broswer.py
+++ #Broswer.py
@@ -2,15 +2,16 @@
 
 import socket
 
-mysock = socket.socket(socket.AF_INET,  socket.SOCK_STREAM)
-mysock.connect(('data.pr4e.org', 80))
-cmd = 'GET http://data.pr4e.org/page1.htm HTTP/1.0\r\n\r\n'.encode()
-mysock.send(cmd)
+mysock = socket.socket(socket.AF_INET,  socket.SOCK_STREAM) #Estabelece um meio de se comunicar
+mysock.connect(('data.pr4e.org', 80)) #Começa a comunicação 
+cmd = 'GET http://data.pr4e.org/page1.htm HTTP/1.0\r\n\r\n'.encode() #Cria uma string com o comando que irá ser mandado
+mysock.send(cmd) #Envia o comando
 
+#Esse loop coleta o 'Data' do documento que pedimos
 while True:
     data=mysock.recv(512)
     if len(data) <1:
         break
     print(data.decode(), end=' ')
 
-mysock.close()
+mysock.close() #Fecha a comunicação 
